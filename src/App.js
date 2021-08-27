5e8e7fa4b8c529eac32c8cc8dad67416fef1637e
srikanth <srikanth.n@evaidya.com>
2021-08-27 15:22:16 +0530

    imageupload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { HashRouter } from 'react-router-dom';
 import './App.css';
+// import { HashRouter } from 'react-router-dom';
+// import ImageLoad from './ImageLoad';
+// import ImageView from './ImageView';
+// import ImageTest from './ImageTest';
+import Navigation from './Navigation';
+import AadharUpload from './AadharUpload';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+     
+        <HashRouter>
+<Navigation></Navigation>  
+</HashRouter>  
+
+{/* <AadharUpload></AadharUpload> */}
+
     </div>
   );
 }
